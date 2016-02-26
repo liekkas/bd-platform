f99b925ff4bc3107010fe2393ff753fb328f7215
liekkas <zjq0717@163.com>
2016-02-26 16:58:50 +0800

    complete route

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,32 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import { App, Home, TVOverview, LiveBroadcast, DemandBroadcast,
-  OverallAnalysis, ChannelAnalysis } from './containers'
+  UserOverview, UserBehave, BusinessOverview,
+  DemandUserAnalysis, MediaAssetsOverview, ShowsTypeAnalysis, ShowsOrderAnalysis,
+  OverallAnalysis, ChannelAnalysis, ChannelGroupAnalysis, ChannelOrder, ShowsOrder } from './containers'
 import { NotFound, About } from './components'
 
 const AppRouter = (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
-    <Route path="tvOverview" component={TVOverview} />
+    <Route path="tvOverview" component={TVOverview}>
+      <IndexRoute component={UserOverview} />
+      <Route path="/tvOverview/userBehave" component={UserBehave} />
+      <Route path="/tvOverview/businessOverview" component={BusinessOverview} />
+    </Route>
     <Route path="liveBroadcast" component={LiveBroadcast}>
       <IndexRoute component={OverallAnalysis} />
+      <Route path="/liveBroadcast/channelGroupAnalysis" component={ChannelGroupAnalysis} />
+      <Route path="/liveBroadcast/channelOrder" component={ChannelOrder} />
       <Route path="/liveBroadcast/channelAnalysis" component={ChannelAnalysis} />
+      <Route path="/liveBroadcast/showsOrder" component={ShowsOrder} />
     </Route>
-    <Route path="demandBroadcast" component={DemandBroadcast} />
+    <Route path="demandBroadcast" component={DemandBroadcast}>
+      <IndexRoute component={DemandUserAnalysis} />
+      <Route path="/demandBroadcast/mediaAssetsOverview" component={MediaAssetsOverview} />
+      <Route path="/demandBroadcast/showsTypeAnalysis" component={ShowsTypeAnalysis} />
+      <Route path="/demandBroadcast/showsOrderAnalysis" component={ShowsOrderAnalysis} />
+    </Route>
     <Route path="*" component={NotFound} status={404}/>
   </Route>
 );
